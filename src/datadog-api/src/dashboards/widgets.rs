f12a0dedac07d434d3e82b54fff20943a09b2c85
Andy Vermeulen <mo@rokt.com>
2021-10-07 16:55:47 +1100

    feat: QueryValue widget

--- src/datadog-api/src/dashboards/widgets.rs
+++ src/datadog-api/src/dashboards/widgets.rs
@@ -6,17 +6,8 @@ use serde::{Serialize, Deserialize};
 pub enum WidgetDefinition {
   #[serde(rename = "note")]
   Note(Note),
-}
-
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub enum WidgetType {
-  #[serde(rename = "note")]
-  Note,
-}
-impl Default for WidgetType {
-  fn default() -> WidgetType {
-    WidgetType::Note
-  }
+  #[serde(rename = "query_value")]
+  QueryValue(QueryValue),
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -49,20 +40,63 @@ impl Default for VerticalAlign {
   }
 }
 
+/// Aggregator used for the request.
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub enum TickEdge {
-  #[serde(rename = "bottom")]
-  Bottom,
-  #[serde(rename = "left")]
-  Left,
-  #[serde(rename = "right")]
-  Right,
-  #[serde(rename = "top")]
-  Top,
+pub enum RequestAggregator {
+  /// Average of all requests.
+  #[serde(rename = "avg")]
+  Average,
+  /// Minimum of all requests.
+  #[serde(rename = "min")]
+  Minimum,
+  /// Maximum of all requests.
+  #[serde(rename = "max")]
+  Maximum,
+  /// Sum of all requests.
+  #[serde(rename = "sum")]
+  Sum,
+  /// Last request value.
+  #[serde(rename = "last")]
+  Last,
+  #[serde(rename = "percentile")]
+  Percentile,
 }
-impl Default for TickEdge {
-  fn default() -> TickEdge {
-    TickEdge::Right
+impl Default for RequestAggregator {
+  fn default() -> RequestAggregator {
+    RequestAggregator::Average
+  }
+}
+
+/// The aggregation methods available for metrics queries.
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub enum MetricAggregator {
+  /// Average of all metrics values.
+  #[serde(rename = "avg")]
+  Average,
+  /// Minimum of all metrics values.
+  #[serde(rename = "min")]
+  Minimum,
+  /// Maximum of all metrics values.
+  #[serde(rename = "max")]
+  Maximum,
+  /// Sum of all metrics values.
+  #[serde(rename = "sum")]
+  Sum,
+  /// Last metrics value.
+  #[serde(rename = "last")]
+  Last,
+  /// Signed area under the curve being graphed, which can be negative
+  #[serde(rename = "area")]
+  Area,
+  /// Uses the norm of the timeseries, which is always positive, to rank the series.
+  #[serde(rename = "l2norm")]
+  Norm,
+  #[serde(rename = "percentile")]
+  Percentile,
+}
+impl Default for MetricAggregator {
+  fn default() -> MetricAggregator {
+    MetricAggregator::Average
   }
 }
 
@@ -100,4 +134,151 @@ impl Default for Note {
       vertical_align: None,
     }
   }
+}
+
+
+/// List of custom links.
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct CustomLink {
+  /// The flag for toggling context menu link visibility.
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub is_hidden: Option<bool>,
+  /// The label for the custom link URL. Keep the label short and descriptive. Use metrics and tags as variables.
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub label: Option<bool>,
+  /// The URL of the custom link. URL must include `http` or `https`. A relative URL must start with `/`.
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub link: Option<bool>,
+  /// The label ID that refers to a context menu link. Can be `logs`, `hosts`, `traces`, `profiles`, `processes`, `containers`, or `rum`.
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub override_label: Option<bool>,
+}
+
+/// Time setting for the widget.
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Time {
+  /// The available timeframes depend on the widget you are using. Allowed enum values: `1m`,`5m`,`10m`,`15m`,`30m`,`1h`,`4h`,`1d`,`2d`,`1w`,`1mo`,`3mo`,`6mo`,`1y`,`alert`
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub live_span: Option<String>, 
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Formula {
+  /// Expression alias.
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub alias: Option<String>,
+  // pub cell_display_mode: String,
+  // pub conditional_formats: String,
+  /// String expression built from queries, formulas, and functions.
+  pub formula: String,
+  // pub limit:
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub enum TickEdge {
+  #[serde(rename = "bottom")]
+  Bottom,
+  #[serde(rename = "left")]
+  Left,
+  #[serde(rename = "right")]
+  Right,
+  #[serde(rename = "top")]
+  Top,
+}
+impl Default for TickEdge {
+  fn default() -> TickEdge {
+    TickEdge::Right
+  }
+}
+
+/// Queries that can be returned directly or used in formulas.
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum Query {
+  Option1(QueryOption1),
+}
+
+/// A formula and functions metrics query.
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct QueryOption1 {
+  /// The aggregation methods available for metrics queries. Allowed enum values: `avg`,`min`,`max`,`sum`,`last`,`area`,`l2norm`,`percentile`
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub aggregator: Option<MetricAggregator>,
+  /// Data source for metrics queries. Allowed enum values: `metrics`
+  pub data_source: String,
+  /// Name of the query for use in formulas.
+  pub name: String,
+  /// Metrics query definition.
+  pub query: String,
+}
+
+/// Timeseries or Scalar response.
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub enum ResponseFormat {
+  #[serde(rename = "timeseries")]
+  Timeseries,
+  #[serde(rename = "scalar")]
+  Scalar,
+}
+
+/// Widget definition.
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Request {
+  /// Aggregator used for the request. Allowed enum values: `avg`,`last`,`max`,`min`,`sum`,`percentile`
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub aggregator: Option<RequestAggregator>,
+  // apm_query
+  // audit_query
+  // conditional_formats
+  // event_query
+  /// List of formulas that operate on queries.
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub formulas: Option<Vec<Formula>>,
+  // log_query
+  // network_query
+  // process_query
+  // profile_metrics_query
+  // q
+  /// List of queries that can be returned directly or used in formulas.
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub queries: Option<Vec<Query>>,
+  /// Timeseries or Scalar response. Allowed enum values: `timeseries`,`scalar`
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub response_format: Option<ResponseFormat>
+  // rum_query
+  // security_query
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct QueryValue {
+  /// Whether to use auto-scaling or not.
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub autoscale: Option<bool>,
+  /// List of custom links.
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub custom_links: Option<Vec<CustomLink>>,
+  /// Display a unit of your choice on the widget.
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub custom_unit: Option<String>,
+  /// Number of decimals to show. If not defined, the widget uses the raw value.
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub precision: Option<i64>,
+  /// Widget definition.
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub requests: Option<Vec<Request>>,
+  /// How to align the text on the widget. Allowed enum values: `center`,`left`,`right`
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub text_align: Option<TextAlign>,
+  /// Time setting for the widget.
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub time: Option<Time>,
+  /// Title of your widget.
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub title: Option<String>,
+  /// How to align the text on the widget. Allowed enum values: `center`,`left`,`right`
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub title_align: Option<TextAlign>,
+  /// Size of the title.
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub title_size: Option<String>,
 }
--- src/datadog-api/tests/dashboards.rs
+++ src/datadog-api/tests/dashboards.rs
@@ -18,35 +18,75 @@ fn test_update_dashboard_empty() {
 #[test]
 fn test_update_dashboard_complete() {
     let client = Client::default();
-    let note = Note {
-        content: "__Objective:__ Ensure classified data is adequately protected\n\n__Indicator:__ AWS S3 Bucket contents is effectively hidden from the broader Internet\n\n---\n\nExport CSV of [all assets](https://meetkunde.eng.roktinternal.com/aws/s3/bucket_effective_access_is_restricted_appropriately) | [non-compliant assets](https://meetkunde.eng.roktinternal.com/aws/s3/bucket_effective_access_is_restricted_appropriately?norm=Violated)".to_string(),
-        background_color: Some("white".to_string()),
-        font_size: Some("14".to_string()),
-        text_align: Some(TextAlign::Left),
-        vertical_align: Some(VerticalAlign::Top),
-        show_tick: Some(false),
-        tick_pos: Some("50%".to_string()),
-        tick_edge: Some(TickEdge::Left),
-        has_padding: Some(true),
-    };
-    let note_layout = WidgetLayout {
-        is_column_break: None,
-        x: 0,
-        y: 0,
-        width: 10,
-        height: 2,
-    };
-    let note_widget = Widget {
+    let note = Widget {
         id: None,
-        definition: Some(WidgetDefinition::Note(note)),
-        layout: Some(note_layout),
+        definition: Some(WidgetDefinition::Note(Note {
+            content: "__Objective:__ Ensure classified data is adequately protected\n\n__Indicator:__ AWS S3 Bucket contents is effectively hidden from the broader Internet\n\n---\n\nExport CSV of [all assets](https://meetkunde.eng.roktinternal.com/aws/s3/bucket_effective_access_is_restricted_appropriately) | [non-compliant assets](https://meetkunde.eng.roktinternal.com/aws/s3/bucket_effective_access_is_restricted_appropriately?norm=Violated)".to_string(),
+            background_color: Some("white".to_string()),
+            font_size: Some("14".to_string()),
+            text_align: Some(TextAlign::Left),
+            vertical_align: Some(VerticalAlign::Top),
+            show_tick: Some(false),
+            tick_pos: Some("50%".to_string()),
+            tick_edge: Some(TickEdge::Left),
+            has_padding: Some(true),
+        })),
+        layout: Some(WidgetLayout {
+            is_column_break: None,
+            x: 0,
+            y: 0,
+            width: 10,
+            height: 2,
+        }),
+    };
+    let queryvalue = Widget {
+        id: None,
+        definition: Some(WidgetDefinition::QueryValue(QueryValue {
+            custom_links: None,
+            custom_unit: None,
+            text_align: None,
+            time: None,
+            title: Some("Total".to_string()),
+            title_size: Some("16".to_string()),
+            title_align: Some(TextAlign::Left),
+            requests: Some(vec!(
+                Request {
+                    aggregator: None,
+                    formulas: Some(vec!(
+                        Formula {
+                            alias: None,
+                            formula: "query1".to_string(),
+                        }
+                    )),
+                    response_format: Some(ResponseFormat::Scalar),
+                    queries: Some(vec!(
+                        Query::Option1(QueryOption1 {
+                            query: "sum:meetkunde.kci.aws.s3.bucket_effective_access_is_restricted_appropriately.gauge{$team}".to_string(),
+                            data_source: "metrics".to_string(),
+                            name: "query1".to_string(),
+                            aggregator: Some(MetricAggregator::Last),
+                        })
+                    ))
+                }
+            )),
+            autoscale: Some(false),
+            precision: Some(0)
+        })),
+        layout: Some(WidgetLayout {
+            is_column_break: None,
+            x: 10,
+            y: 0,
+            width: 2,
+            height: 1
+        }),
     };
     let req = UpdateDashboardRequest::default()
         .dashboard_id("v2g-k58-bhe")
         .title("datadog-api-rs: test_update_dashboard_complete")
         .layout_type(LayoutType::Ordered)
         .widgets(vec!(
-            note_widget
+            note,
+            queryvalue,
         ));
     let res = block_on(req.send(&client)).expect("API call failed");
     // assert_ne!(0, res.status);
